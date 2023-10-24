407274f57cbae840c4838bc6a59187cc11fdd36c
amirn003 <amir.naar@gmail.com>
2023-10-24 15:37:44 +0200

    add playing style

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 window.addEventListener('keydown', function (e) {
-  //console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return;
+  audio.currentTime = 0;
   audio.play();
+  key.classList.toggle('playing');
 });
