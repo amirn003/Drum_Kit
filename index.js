63b593f32da92689ab507d36f4e4735abe3e926c
amirn003 <amir.naar@gmail.com>
2023-10-24 15:58:53 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-window.addEventListener('keydown', function (e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add('playing');
-});
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
 
 const keys = document.querySelectorAll(".key")
 //console.log(keys)
 
 function removeTransition(e) {
-
   if(e.propertyName !== 'transform') return;
-
-  console.log(this);
+  //console.log(this);
   this.classList.remove("playing");
 }
 
 keys.forEach((key) => {
   key.addEventListener("transitionend", removeTransition)
 })
+
+window.addEventListener('keydown', playSound);
