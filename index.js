ec0c8373a06791694dd9e63447d8d4afcd2df820
amirn003 <amir.naar@gmail.com>
2023-10-24 15:31:32 +0200

    play audio element

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ window.addEventListener('keydown', function (e) {
   //console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   console.log(audio);
+  if(!audio) return;
+  audio.play();
 });
